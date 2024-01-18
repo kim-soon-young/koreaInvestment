ae049be51ca1d21beafa197f7653978e853c94b3
young <makt00byoung@gmail.com>
2024-01-18 18:14:25 +0900

    2단->4단으로 완성

--- js/main.js
+++ js/main.js
@@ -2,11 +2,19 @@ $(document).ready(function () {
  
   AOS.init();
 
-  // 토글 버튼 눌렀을 때 메뉴 내려와라
+  //햄버거를 누르면 오른쪽에서 메뉴가 나와라.
   var menuToggleBtn = $(".menu-toggle-btn");
-  mainMenu = $(".main-menu1");
+  var mainMenu = $(".main-menu1");
+
   menuToggleBtn.click(function () {
-    mainMenu.slideToggle();
+    mainMenu.animate({ right: '0' }, 300,); // 오른쪽에서 슬라이딩
+  });
+
+  // 메뉴 외부를 클릭하면 메뉴를 닫습니다.
+  $(document).mouseup(function (e) {
+    if (!mainMenu.is(e.target) && mainMenu.has(e.target).length === 0) {
+      mainMenu.animate({ right: '-250px' }, 300); // 오른쪽으로 슬라이딩해서 닫기
+    }
   });
 
 });
